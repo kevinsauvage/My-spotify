a82b22fc82fbcc3878c009278c63412c3dc319a5
kevin sauvage <kevinsauvage@outlook.com>
2021-06-04 17:40:19 +0200

    add tracks component to bibliotheque

--- src/route/bibliotheque/Bibliotheque.jsx
+++ src/route/bibliotheque/Bibliotheque.jsx
@@ -2,10 +2,9 @@ import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
 import "./Bibliotheque.scss";
 import Loader from "react-loader-spinner";
-import BibliothequeItem from "../../components/bibliothequeItem/BibliothequeItem";
 import PlayBtn from "../../components/playBtn/PlayBtn";
 import { AnimatePresence, motion } from "framer-motion";
-import BibliothequeItemHeader from "../../components/bibliothequeItemHeader/BibliothequeItemHeader";
+import Tracks from "../../components/tracks/Tracks";
 
 const Bibliotheque = () => {
   const {
@@ -16,7 +15,6 @@ const Bibliotheque = () => {
     playlistToPlay,
     setPlaylistUri,
     tracks,
-    millisToMinutesAndSeconds,
   } = useContext(AppContext);
 
   useEffect(() => {
@@ -58,47 +56,7 @@ const Bibliotheque = () => {
                 {playlistToPlay && <PlayBtn onClick={setPlaylistUri} />}
               </div>
             </div>
-            <div className="bibliotheque__tracks">
-              <div className="section-header">
-                <BibliothequeItemHeader name artist duration play queu />
-              </div>
-              {tracks &&
-                tracks.map((track) => {
-                  return (
-                    <BibliothequeItem
-                      key={
-                        track.id + Math.random(1000) ||
-                        track.track.id + Math.random(1000)
-                      }
-                      name={track.name || track.track.name}
-                      artist={
-                        track.track
-                          ? track.track.artists[0].name
-                          : track.album
-                          ? track.album.artists[0].name
-                          : track.artists
-                          ? track.artists[0].name
-                          : null
-                      }
-                      duration={millisToMinutesAndSeconds(
-                        track.duration_ms || track.track.duration_ms
-                      )}
-                      artistId={
-                        track.track
-                          ? track.track.artists[0].id
-                          : track.album
-                          ? track.album.artists[0].id
-                          : track.artists
-                          ? track.artists[0].id
-                          : null
-                      }
-                      uri={track.track ? track.track.uri : track.uri}
-                      id={track.id || track.track.id}
-                      play
-                    />
-                  );
-                })}
-            </div>
+            <Tracks data={tracks} />
           </>
         )}
       </AnimatePresence>
