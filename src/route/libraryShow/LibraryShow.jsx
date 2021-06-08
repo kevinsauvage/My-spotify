8a8d9413095ef6b8be91d2af57252d39b1441ebc
kevin sauvage <kevinsauvage@outlook.com>
2021-06-08 23:09:15 +0200

    refactor library show

--- src/route/libraryShow/LibraryShow.jsx
+++ src/route/libraryShow/LibraryShow.jsx
@@ -7,9 +7,7 @@ import { AppContext } from "../../context/AppContext";
 const LibraryShow = () => {
   const location = useLocation();
   const { id } = location.state;
-  const [topTracks, setTopTracks] = useState();
-  const [likedTracks, setLikedTracks] = useState();
-  const [recentlyPlayedTracks, setRecentlyPlayedTracks] = useState();
+  const [tracks, setTracks] = useState();
   const { spotifyApi, scrollbar, handleSidebarMenu, setUri } =
     useContext(AppContext);
 
@@ -18,12 +16,12 @@ const LibraryShow = () => {
       handleSidebarMenu();
       const response = await spotifyApi.getMyTopTracks({ limit: 50 });
       const tracks = response.items;
-      setTopTracks(tracks);
+      setTracks(tracks);
     };
     if (id === "Top Tracks") {
       getTopTracks();
-    } else setTopTracks(undefined);
-  }, [spotifyApi, setTopTracks, id, scrollbar, handleSidebarMenu]);
+    }
+  }, [spotifyApi, id, scrollbar, handleSidebarMenu]);
 
   useEffect(() => {
     const getLikedTracks = async () => {
@@ -32,12 +30,12 @@ const LibraryShow = () => {
         limit: 50,
       });
       const tracks = response.items.map((item) => item.track);
-      setLikedTracks(tracks);
+      setTracks(tracks);
     };
     if (id === "Liked Tracks") {
       getLikedTracks();
-    } else setLikedTracks(undefined);
-  }, [spotifyApi, setLikedTracks, id, scrollbar, handleSidebarMenu]);
+    }
+  }, [spotifyApi, id, scrollbar, handleSidebarMenu]);
 
   useEffect(() => {
     const getRecentlyPlayed = async () => {
@@ -47,27 +45,17 @@ const LibraryShow = () => {
         limit: 50,
       });
       const tracks = response.items.map((item) => item.track);
-      setRecentlyPlayedTracks(tracks);
+      setTracks(tracks);
     };
     if (id === "Recently Played") {
       getRecentlyPlayed();
-    } else setRecentlyPlayedTracks(undefined);
-  }, [spotifyApi, setRecentlyPlayedTracks, id, scrollbar, handleSidebarMenu]);
+    }
+  }, [spotifyApi, id, scrollbar, handleSidebarMenu]);
 
   const handleClickPlay = useCallback(() => {
-    if (topTracks) {
-      const uris = topTracks.map((track) => track.uri);
-      setUri(uris);
-    }
-    if (recentlyPlayedTracks) {
-      const uris = recentlyPlayedTracks.map((track) => track.uri);
-      setUri(uris);
-    }
-    if (likedTracks) {
-      const uris = likedTracks.map((track) => track.uri);
-      setUri(uris);
-    }
-  }, [likedTracks, recentlyPlayedTracks, setUri, topTracks]);
+    const uris = tracks.map((track) => track.uri);
+    setUri(uris);
+  }, [setUri, tracks]);
 
   const bg =
     "url(https://cdn.pixabay.com/photo/2013/07/12/18/17/equalizer-153212_960_720.png)";
@@ -75,7 +63,7 @@ const LibraryShow = () => {
   return (
     <div className="bibliotheque">
       <PageBanner onClick={handleClickPlay} title={id} bg={bg} />
-      <Tracks data={topTracks || likedTracks || recentlyPlayedTracks} />
+      <Tracks data={tracks} />
     </div>
   );
 };
