9041f21f7c8b5476f7081a5a4423e0d2a4f3597f
kevin sauvage <kevinsauvage@outlook.com>
2021-06-09 20:19:36 +0200

    change font and size carusel title

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import "./Card.scss";
 const Card = ({ url, name, artist }) => {
   return (
     <motion.div
-      whileHover={{ scale: 1.3 }}
+      whileHover={{ scale: 1.1 }}
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}>
       <div
